cf64b044c37498c68445c8aac6213ef4765e8bd6
“Dongdonghe1981” <43654280@qq.com>
2019-11-23 08:24:51 +0800

    mod

--- src/streamapi/StreamEndTest.kt
+++
@@ -1,4 +0,0 @@
-package streamapi
-
-class StreamEndTest {
-}
---
+++ src/streamapi/StreamTest.java
@@ -0,0 +1,16 @@
+package streamapi;
+
+import dto.Employee;
+import dto.EmployeeData;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest {
+    @Test
+    public void test1() {
+        List<Employee> emptiest = EmployeeData.getEmployee();
+
+    }
+}
